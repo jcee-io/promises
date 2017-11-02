50cbc310c15f184a9849b4fd94657647fc8f8b1f
jcruzz <jvcruz@ucdavis.edu>
2017-11-01 20:08:21 -0700

    cron

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,13 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archive = require('../helpers/archive-helpers');
+var CronJob = require('cron').CronJob;
+var fs = require('fs');
+
+var job = new CronJob('* * * * * *', function() {
+  archive.readListOfUrls(data => {
+    archive.downloadUrls(data);
+  }); 
+}, true);
+
+job.start();
